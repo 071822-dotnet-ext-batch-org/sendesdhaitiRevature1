6dd9bf60bfeafb4e7b869a933e5834004efba8fa
sendes <sdhaiti.business@gmail.com>
2022-08-15 11:12:02 -0400

    RP1 - The commit to fix the ADO.net package dependency

---
+++ REV_PROJECTS/Rev_P1_2/ReImbursementApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BusinessLayer;
+
+namespace ReImbursementApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Messages.Welcome("Welcome to the Revature ReImbursement System.");
+            RunAppSession myAppSession = new RunAppSession();
+
+            //Run a session
+            myAppSession.NewAppSession();
+            myAppSession.AddNewEmployee();
+            
+
+
+
+        }
+    }
+}
